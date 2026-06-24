37d139ab2cc23afe3e7de29c011cfb08b26c5111
Pratham <infinity181920@gmail.com>
2026-06-24 17:54:18 +0530

    Refactor json response in onRequestGet function

--- functions/get_sheets.js
+++ functions/get_sheets.js
@@ -1,14 +1,13 @@
 export async function onRequestGet(context) {
-  return json({
-    sheets: ["Sheet1", "Questions", "Test"]
-  });
-}
-
-function json(data, status = 200) {
-  return new Response(JSON.stringify(data), {
-    status,
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  return new Response(
+    JSON.stringify({
+      sheets: ["Sheet1", "Questions", "Test"]
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+  );
 }
