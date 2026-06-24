6d18cb6b85dc6ad1b9fa0cc0121fe7d34842d20f
Pratham <infinity181920@gmail.com>
2026-06-24 17:45:19 +0530

    Update get_sheets function to return sheets list

--- functions/get_sheets.js
+++ functions/get_sheets.js
@@ -1,12 +1,14 @@
 export async function onRequestGet(context) {
   return json({
-    error: "The /get_sheets function is not connected yet."
-  }, 501);
+    sheets: ["Sheet1", "Questions", "Test"]
+  });
 }
 
 function json(data, status = 200) {
   return new Response(JSON.stringify(data), {
     status,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+    },
   });
 }
