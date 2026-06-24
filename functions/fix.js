b917b5a7123a8e7aa212cc932d448f25c8fce60b
Pratham <infinity181920@gmail.com>
2026-06-24 17:38:35 +0530

    Add /fix function with error response

---
+++ functions/fix.js
@@ -0,0 +1,12 @@
+export async function onRequestPost(context) {
+  return json({
+    error: "The /fix function is not connected yet."
+  }, 501);
+}
+
+function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: { "Content-Type": "application/json" },
+  });
+}
